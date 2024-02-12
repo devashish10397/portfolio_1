5c410f0a0e07ac01e702641aba6093619bdf1730
devashish10397 <devashishubale10397@gmail.com>
2024-02-12 13:45:00 -0500

    Changed the welcome message

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,9 +14,9 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
+                Hi there!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
+                  🙋‍♂️
                 </span>
               </h1>
 
