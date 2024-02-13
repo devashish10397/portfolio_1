03ba8fc127cd7b3ca7d0fed2ba40da9c9ad9e173
devashish10397 <devashishubale10397@gmail.com>
2024-02-12 20:11:20 -0500

    repalce copywright by tm

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ function Footer() {
           <h3>Designed and Developed by Devashish</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} DEV</h3>
+          <h3>tm © {year} DEV</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
