a9fa9604ff59f79205285d050f7a715acceeef4a
devashish10397 <devashishubale10397@gmail.com>
2024-02-15 16:06:52 -0500

    checking in the changes to master

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
-<<<<<<< HEAD
-  
   AiFillMail,
 } from "react-icons/ai";
+
 import { FaLinkedinIn } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
-=======
-  AiFillMail,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
->>>>>>> main
 import { AiFillInstagram } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
 
@@ -26,21 +20,13 @@ function Footer() {
           <h3>Designed and Developed by Devashish</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-<<<<<<< HEAD
-          <h3>Copyright © {year} SG</h3>
-=======
           <h3>tm © {year} DEV</h3>
->>>>>>> main
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
-            {<li className="social-icons">
+            <li className="social-icons">
               <a
-<<<<<<< HEAD
-                href="https://github.com/devashish103"
-=======
                 href="https://github.com/devashish10397"
->>>>>>> main
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -48,7 +34,6 @@ function Footer() {
                 <AiFillGithub />
               </a>
             </li>
-            }
             <li className="social-icons">
               <a
                 href="mailto:devashishuable10@gmail.com"
@@ -69,7 +54,17 @@ function Footer() {
                 <FaLinkedinIn />
               </a>
             </li>
-            {<li className="social-icons">
+            <li className="social-icons">
+              <a
+                href="https://leetcode.com/devis10/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <SiLeetcode />
+              </a>
+            </li>
+            <li className="social-icons">
               <a
                 href="https://www.instagram.com/dev_leetcode_journey/"
                 style={{ color: "white" }}
@@ -78,7 +73,7 @@ function Footer() {
               >
                 <AiFillInstagram/>
               </a>
-            </li>}
+            </li>
           </ul>
         </Col>
       </Row>
